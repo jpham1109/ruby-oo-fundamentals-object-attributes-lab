fb139c3cf6104efc4513f2f27c458c76ce5cc17d
Jamie H Pham <jpham1109@gmail.com>
2021-02-02 12:59:18 -0500

    commit tree e023784eb121a71debd96eefe911c0c5c7d97584

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def breed=(breed)
+        @breed = breed 
+    end 
+
+    def breed 
+        @breed 
+    end 
+
+    def name=(dog_name)
+        @name = dog_name
+    end 
+
+    def name
+        @name 
+    end
+end 
